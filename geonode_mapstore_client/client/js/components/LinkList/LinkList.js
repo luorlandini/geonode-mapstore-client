8a700116f5c1e5979761fe5e55c31ad418b4554e
luorlandini <luca.orlandini@gmail.com>
2021-03-24 21:16:59 +0100

    links resource in filters

---
+++ geonode_mapstore_client/client/js/components/LinkList/LinkList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LinkList = ({ children, classes, ...props }) => {
+
+  return (
+    <ul>
+      {children}
+    </ul>
+  )
+
+}
+
+LinkList.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  props: PropTypes.object,
+  classes: PropTypes.string
+};
+
+export default LinkList;
---
+++ geonode_mapstore_client/client/js/components/home/FilterLinks.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import LinkList from '../LinkList';
+import LinkItem from '../LinkItem';
+import Message from '@mapstore/framework/components/I18N/Message';
+import { Glyphicon } from 'react-bootstrap';
+import FaIcon from '@js/components/home/FaIcon';
+
+const FilterLink = ({className, blockName, items}) => (
+    <div className={className}>
+        <strong>{blockName}</strong>
+        <LinkList>
+            {
+                items.map((item, index) => (
+                        <LinkItem
+                        key={index}
+                        name={<Message msgId={item.labelId} />}
+                        linkTo={item.href} >
+                            <FaIcon name="database"/>
+                        </LinkItem>
+                ))
+            }
+        </LinkList>
+      </div>
+);
+
+export default FilterLink;
