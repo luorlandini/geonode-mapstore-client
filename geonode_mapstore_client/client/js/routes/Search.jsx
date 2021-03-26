c21a1624bd39c2ec10ec668633ea6d66d2223142
luorlandini <luca.orlandini@gmail.com>
2021-03-26 13:25:31 +0100

    rm global toggle state

--- geonode_mapstore_client/client/js/routes/Search.jsx
+++ geonode_mapstore_client/client/js/routes/Search.jsx
@@ -1,16 +1,8 @@
 
-import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
-import  { toggleFilter }  from '@js/actions/gnfilters';
 import Home from '@js/routes/Home';
 
-const SearchRoute = connect(
-    createSelector([
-        state => state?.gnfilters?.isToggle || false
-    ], (isToggle) => ({
-        isToggle
-    })),
-    () => ({ hideHero: true, isToggle:false, onToggleFilter: toggleFilter })
+const SearchRoute = connect(() => ({ hideHero: true })
 )((Home));
 
 export default SearchRoute;
