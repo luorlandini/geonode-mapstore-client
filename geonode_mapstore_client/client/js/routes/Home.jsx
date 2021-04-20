69a1595bf1af5c11e58dda2e590e5f9a49015ebd
luorlandini <luca.orlandini@gmail.com>
2021-04-20 10:34:29 +0200

    add param cards Menu

--- geonode_mapstore_client/client/js/routes/Home.jsx
+++ geonode_mapstore_client/client/js/routes/Home.jsx
@@ -191,6 +191,7 @@ function Home({
     isToggle,
     menu,
     navbar,
+    cardsMenu,
     footer,
     hideHero,
     isFilterForm,
@@ -209,7 +210,6 @@ function Home({
             isMounted.current = false;
         };
     }, []);
-
 
     const brandNavbarNode = useRef();
     const menuIndexNode = useRef();
@@ -488,7 +488,7 @@ function Home({
                                         top: dimensions.brandNavbarHeight + dimensions.menuIndexNodeHeight
                                     }}
                                     formatHref={handleFormatHref}
-                                    actions={filters?.actions}
+                                    cardsMenu={cardsMenu?.items}
                                     order={query?.sort}
                                     filters={queryFilters}
                                     onClear={handleClear}
