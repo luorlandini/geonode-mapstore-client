15f55a4ae0eec4331ea5af2fe277317e4f095c58
luorlandini <luca.orlandini@gmail.com>
2021-04-21 12:54:15 +0200

    import dropdonwlist

--- geonode_mapstore_client/client/js/components/Menu/MenuItem.js
+++ geonode_mapstore_client/client/js/components/Menu/MenuItem.js
@@ -10,60 +10,37 @@ import {
     readProperty,
     filterMenuItems
 } from '@js/utils/MenuUtils';
-
+import { Nav } from 'react-bootstrap-v1';
+import DropdownList from './DropdownList';
 const isValidBadgeValue = value => !!(value !== '' && !isNil(value));
-
 const MenuItem = ({item, menuItemsProps, containerNode, tabIndex, draggable, dropdownClass}) => {
 
     const { formatHref, query, state } = menuItemsProps;
-    const { type, label, labelId = '', items = [], href, style, badge = '', image } = item;
+    const { id, type, label, labelId = '', items = [], href, style, badge = '', image } = item;
     const badgeValue = readProperty(state, badge);
+
     if (type === 'dropdown') {
-        const dropdownItems = items
-            .filter((itm) => filterMenuItems(state, itm, item))
-            .map((itm, idx) => {
-                if (itm.type === 'divider') {
-                    return <Dropdown.Divider key={idx} />;
-                }
-                const itmBadgeValue = readProperty(state, itm.badge || '');
-                return (
-                    <Dropdown.Item
-                        key={idx}
-                        href={readProperty(state, itm.href)}
-                        style={itm.style}
-                    >
-                        {itm.labelId && <Message msgId={itm.labelId}/> || itm.label}
-                        {isValidBadgeValue(itmBadgeValue) && <Badge>{itmBadgeValue}</Badge>}
-                    </Dropdown.Item>
-                );
-            });
+        return (<DropdownList
+            id={id}
+            items={items}
+            label={label}
+            labelId={labelId}
+            toogleStyle={style}
+            toogleImage={image}
+            state={state}
+            dropdownClass={dropdownClass}
+            tabIndex={tabIndex}
+            badgeValue={badgeValue}
+            containerNode={containerNode}
+        />)
+    }
+    /*
+    if (type === 'link') {
         return (
-            <Dropdown
-                className={`${dropdownClass}`}
-            >
-                <Dropdown.Toggle
-                    id={'gn-menu-index-' + item.id}
-                    variant="default"
-                    tabIndex={tabIndex}
-                    style={style}
-                >
-                    {image
-                        ? <img src={readProperty(state, image)} />
-                        : null
-                    }
-                    {labelId && <Message msgId={labelId}/> || label}
-                    {isValidBadgeValue(badgeValue) && <Badge>{badgeValue}</Badge>}
-                </Dropdown.Toggle>
-                {containerNode
-                    ? createPortal(<Dropdown.Menu>
-                        {dropdownItems}
-                    </Dropdown.Menu>, containerNode.parentNode)
-                    : <Dropdown.Menu>
-                        {dropdownItems}
-                    </Dropdown.Menu>}
-            </Dropdown>
+            <Nav.Link href={readProperty(state, href)}>{labelId && <Message msgId={labelId}/> || label}</Nav.Link>
         );
     }
+    */
     if (type === 'link') {
         return (
             <Tag
@@ -71,6 +48,7 @@ const MenuItem = ({item, menuItemsProps, containerNode, tabIndex, draggable, dro
                 draggable={draggable}
                 href={readProperty(state, href)}
                 style={style}
+
             >
                 {labelId && <Message msgId={labelId}/> || label}
                 {isValidBadgeValue(badgeValue) && <Badge>{badgeValue}</Badge>}
