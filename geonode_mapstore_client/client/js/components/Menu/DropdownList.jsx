be7e96dfd9ca90fcf4588d4d3ae58d93beb19173
luorlandini <luca.orlandini@gmail.com>
2021-04-21 17:29:09 +0200

    add license

--- geonode_mapstore_client/client/js/components/Menu/DropdownList.jsx
+++ geonode_mapstore_client/client/js/components/Menu/DropdownList.jsx
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2021, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Message from '@mapstore/framework/components/I18N/Message';
