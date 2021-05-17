d8a94df578da94b676e6fd0f6f710f171479fa9d
Luca Orlandini <luca.orlandini@gmail.com>
2021-05-17 12:16:38 +0200

    editable content

--- geonode_mapstore_client/client/js/plugins/DetailViewer.jsx
+++ geonode_mapstore_client/client/js/plugins/DetailViewer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-boolean-value */
 
 /*
  * Copyright 2021, GeoSolutions Sas.
@@ -7,21 +8,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { createPlugin } from '@mapstore/framework/utils/PluginsUtils';
 import { connect } from 'react-redux';
+import FaIcon from '@js/components/home/FaIcon';
 import { createSelector } from 'reselect';
 import DetailsPanel from '@js/components/home/DetailsPanel';
-import DropdownList from '@js/components/Menu/DropdownList';
-
-const DetailViewerMenuItem = ({items}) => {
-
-    return (
-        <DropdownList
-                items={items}
-            />
-    )
-}
+import {TextEditable, RichTextEditable, ImagesEditable} from '@js/components/ContentsEditable/';
+import { editResource } from '@js/actions/gnresource';
+import gnresource from '@js/reducers/gnresource';
 
 
 const ConnectedDetailsPanel = connect(
@@ -35,23 +30,72 @@ const ConnectedDetailsPanel = connect(
 )(DetailsPanel);
 
 
-function DetailViewer({}) {
-    // placeholder plugin component
+function DetailViewer({onEditResource}) {
+
+    const [editMode, setEditMode] = useState(false);
+    const [iconEditMode,  iconSetEditMode] = useState('edit');
+
+    const onChangeValue = (val) => {
+        onEditResource(val);
+        // console.log(val);
+    };
+
+    const editTitle = (value) => {
+        return (<h1><TextEditable  onEdit={ onChangeValue } text={value} /></h1>);
+    };
+
+    const editAbstract = (abstract) => (
+        <div className="editContainer">
+            <TextEditable onEdit={ onChangeValue } text={abstract} />
+        </div>
+
+    );
+
+    const editImage = (image) => (
+        <div className="editContainer imagepreview">
+            <ImagesEditable defaultImage={image} />
+        </div>
+
+    );
+
+    const handleEditMode = () => {
+        setEditMode(!editMode);
+    };
+
+    useEffect(() => {
+
+        (editMode) ? iconSetEditMode('eye') : iconSetEditMode('edit');
+
+    }, [editMode]);
+
     return (
         <div
             style={{
                 top: 0,
                 left: 0,
                 width: '100%',
-                height: '100%',
-                backgroundColor: '#f2f0f0',
+
             }}>
+            <div
+                className={`gn-edit-toogle`}
+                style={{
+                    display: 'flex',
+                    flex: 1
+                }}  >
+                <button onClick={handleEditMode} type="button" className="btn" >
+                    <FaIcon name={iconEditMode} />
+                </button>
+            </div>
             <ConnectedDetailsPanel
+                editable={editMode}
+                editTitle={editTitle}
+                editAbstract={editAbstract}
+                editImage={editImage}
                 sectionStyle={{
-                        width: '600px',
-                        position: 'fixed',
-                        overflowY: 'scroll',
-                        height: 'auto'
+                    width: '600px',
+                    position: 'fixed',
+                    overflowY: 'scroll',
+                    height: 'auto'
 
                 }}
             />
@@ -60,8 +104,11 @@ function DetailViewer({}) {
 }
 
 const DetailViewerPlugin = connect(
-    createSelector([], () => ({})),
-    {}
+    createSelector([
+    ], () => ({})),
+    {
+        onEditResource: editResource
+    }
 )(DetailViewer);
 
 
@@ -75,12 +122,13 @@ export default createPlugin('DetailViewer', {
         }
     },
     epics: {},
-    reducers: {}
+    reducers: {
+        gnresource
+    }
 });
 
 
-
-        /*
+/*
         ActionNavbar: {
             name: 'DetailViewerMenuItem',
             target: 'dropdown.edit', // we should find a way to target an existing dropdown
@@ -88,3 +136,24 @@ export default createPlugin('DetailViewer', {
             priority: 1
         }
         */
+
+
+/*
+    { editable && <span onClick={startEditingTitle} ><FaIcon name={'edit'} /></span>}
+
+                        {isEditingTitle &&
+                            <div className="editContainer">
+                                <h1><TextEditable ref={editTitleRef} text={resource?.title} /></h1>
+                            </div>
+                        }
+
+                        { editable && !isEditingAbstract && <span onClick={startEditingAbstract} ><FaIcon name={'edit'} /></span>}
+
+
+                            {isEditingAbstract &&
+                            <div className='editContainer'>
+                                <TextEditable ref={editAbstractRef} text={resource?.abstract} />
+                            </div>
+                            }
+
+*/
