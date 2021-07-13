8343fb88f78233cc482a821579425d67780c0f87
Luca Orlandini <luca.orlandini@gmail.com>
2021-07-13 12:25:43 +0200

    rm debug

--- geonode_mapstore_client/client/js/components/Menu/MenuItem.js
+++ geonode_mapstore_client/client/js/components/Menu/MenuItem.js
@@ -44,8 +44,6 @@ const MenuItem = ({ item, menuItemsProps, containerNode, tabIndex, draggable, cl
 
     const { formatHref, query } = menuItemsProps;
     const { id, type, label, labelId = '', items = [], href, style, badge = '', image, Component, target } = item;
-    console.log('item');
-    console.log(item);
     const badgeValue = badge;
     if (type === 'dropdown') {
         return (<DropdownList
