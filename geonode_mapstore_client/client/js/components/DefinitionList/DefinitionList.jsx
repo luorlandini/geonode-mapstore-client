95cdfa0d6428110321ca366fd8715dbc1bb9c1d2
Luca Orlandini <luca.orlandini@gmail.com>
2021-07-19 14:21:09 +0200

    check value

--- geonode_mapstore_client/client/js/components/DefinitionList/DefinitionList.jsx
+++ geonode_mapstore_client/client/js/components/DefinitionList/DefinitionList.jsx
@@ -5,10 +5,11 @@ const DefinitionList = ({itemslist}) => {
 
     const items = itemslist?.map( item => {
         return (
-            <>
+            item.value && <>
                 <dt className="DList-cell DList-cell--medium"><strong>{`${item.label}:`}</strong></dt>
                 <dd className="DList-cell">{`${item.value}`}</dd>
             </>
+
         );
     });
 
