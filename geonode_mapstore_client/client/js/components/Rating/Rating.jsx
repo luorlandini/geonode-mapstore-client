3bc4b60a4f4a7ccc411ddef721857427069c332c
Luca Orlandini <luca.orlandini@gmail.com>
2021-08-20 17:28:56 +0200

    add rating component

---
+++ geonode_mapstore_client/client/js/components/Rating/Rating.jsx
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2021, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import Rating from 'react-rating';
+
+export default Rating;
