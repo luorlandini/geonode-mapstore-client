770b91b7a1e4443f3b443bf0d25447d23e84cc51
Luca Orlandini <luca.orlandini@gmail.com>
2021-08-23 11:35:10 +0200

    lint

--- geonode_mapstore_client/client/js/epics/rating.js
+++ geonode_mapstore_client/client/js/epics/rating.js
@@ -12,18 +12,19 @@ import {
     updateResourceProperties,
     SET_RATING_RESOURCES
 } from '@js/actions/gnresource';
+/*
 import {
     setRatingResource
 } from '@js/api/geonode/v2';
-
+*/
 const gnSaveRatingContent = (action$, store) =>
     action$.ofType(SET_RATING_RESOURCES)
         .switchMap((action) => {
-            const state = store.getState();
-            const pk = state?.gnresource?.data.pk;
+            // const state = store.getState();
+            // const pk = state?.gnresource?.data.pk;
             const rating =  action.rating;
             return Observable
-                //.defer(() => setRatingResource(pk, rating))
+                // .defer(() => setRatingResource(pk, rating))
                 .defer(() => console.log(`rating ${rating}`))
                 .switchMap(() => {
                     return Observable.of(
